5481901c05631d7e485da68f28b5fb42ffe69f85
unknown <Jenyojohn24@gmail.com>
2020-04-14 14:36:18 +1245

    bug fix

--- src/estimator.js
+++ src/estimator.js
@@ -1,20 +1,18 @@
 const covid19ImpactEstimator = (data) => {
-    data;
-
-    //Normalize period type
-    let normalized_day = (period, timeToElapse) => {
+    //  Normalize period type
+    const normalizedDay = (period, timeToElapse) => {
         timeToElapse = data.timeToElapse;
-        switch(period){
-            case "days":
+        switch (period) {
+            case 'days':
                 return timeToElapse;
-            case "weeks":
+            case 'weeks':
                 return timeToElapse * 7;
             default:
                 return timeToElapse * 30;      
         }
     }
 
-    //Calculate currently infected which doubles in size every 3 days
+    //  Calculate currently infected which doubles in size every 3 days
     infectionAfterDoubling = (timeToElapse, currentlyInfected) => {
         power = Math.floor(timeToElapse/3)
         return currentlyInfected*(Math.pow(2,power))
@@ -29,20 +27,33 @@ const covid19ImpactEstimator = (data) => {
             //Number of currently infected people
             currentlyInfected: data.reportedCases * 10,
             //Number of infected people in 30 days
-            infectionsByRequestedTime: infectionAfterDoubling(normalized_day(data.periodType, data.timeToElapse), this.currentlyInfected)
+            infectionsByRequestedTime: infectionAfterDoubling(normalizedDay(data.periodType, data.timeToElapse), this.currentlyInfected)
         }, // your best case estimation
         severeImpact: {
             //Number of currently infected people
             currentlyInfected: data.reportedCases * 50,
             //Number of infected people in 30 days
-            infectionsByRequestedTime: infectionAfterDoubling(normalized_day(data.periodType, data.timeToElapse), this.currentlyInfected)    
+            infectionsByRequestedTime: infectionAfterDoubling(normalizedDay(data.periodType, data.timeToElapse), this.currentlyInfected)    
         } // your severe case estimation
     }
     
 }
 
+myData = {
+    region: {
+    name: "Africa",
+    avgAge: 19.7,
+    avgDailyIncomeInUSD: 5,
+    avgDailyIncomePopulation: 0.71
+    },
+    periodType: "days",
+    timeToElapse: 58,
+    reportedCases: 674,
+    population: 66622705,
+    totalHospitalBeds: 1380614
+    }
 
+covid19ImpactEstimator(myData)
 
     
-module.export
-//export default covid19ImpactEstimator;
+export default covid19ImpactEstimator;
